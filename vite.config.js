8a802e9d41a3c397beeb84e798bf1e3f92d156a6
korinne <korinne@cloudflare.com>
2025-03-19 12:56:16 -0700

    adds local connection string for hyperdrive

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cloudflare({
-      configPath: "./wrangler.jsonc",
-      auxiliaryWorkers: [
-        { 
-          configPath: "./test-worker/wrangler.jsonc"
-        }
-      ],
-      persistState: false,
-    }),
-  ],
+  plugins: [react(), cloudflare()],
 });
